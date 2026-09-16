0bf73534e266473db466de2f571b86d228f9031c
amaan-py <70054364+amaan-py@users.noreply.github.com>
2026-09-16 22:53:13 +0530

    Create build.gradle.kts

---
+++ src/en/manhwaz/build.gradle.kts
@@ -0,0 +1,21 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "ManhwaZ"
+    versionCode = 1
+    contentWarning = ContentWarning.MIXED
+    libVersion = "1.6"
+
+    source {
+        lang = "en"
+        baseUrl = "https://www.manhwaz.cc"
+    }
+
+    deeplink {
+        path("/webtoon/..*")
+    }
+}
